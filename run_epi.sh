7caaf99b15473e160a84fd1bd40045e4a96d69b7
americast <sayan.sinha@cc.gatech.edu>
2023-12-05 20:18:52 -0500

    rearrange

--- run_epi.sh
+++
@@ -1,7 +0,0 @@
-for data in covid_deaths hospital cdc_flu cdc_covid symp
-do
-    for model in llama-7b gpt4all arima
-    do
-        CUDA_VISIBLE_DEVICES="0" python -m experiments.run_epi --dataset $data --model $model
-    done
-done
